e032cd46e66889f40d5660b65efe26c405e7e06e
TarsisViana <159029529+TarsisViana@users.noreply.github.com>
2024-06-08 13:52:40 +0100

    Add insert and remove functions

--- main.mjs
+++ main.mjs
@@ -39,8 +39,58 @@ class Tree {
 
     return toBBST(orderedArr, 0, orderedArr.length - 1);
   }
+
+  insert(value, root = this.root) {
+    if (root == null) {
+      root = new Node(value);
+      return root;
+    } else if (root.value > value) {
+      root.left = this.insert(value, root.left);
+    } else if (root.value < value) {
+      root.right = this.insert(value, root.right);
+    }
+
+    return root;
+  }
+
+  deleteItem(value, root = this.root) {
+    if (root.value == null) {
+      return root;
+    }
+
+    if (value < root.value) {
+      root.left = this.deleteItem(value, root.left);
+    } else if (value > root.value) {
+      root.right = this.deleteItem(value, root.right);
+    } else {
+      if (root.left == null) {
+        return root.right;
+      }
+      if (root.right == null) {
+        return root.left;
+      }
+      root.value = this.minValue(root.right);
+
+      root.right = this.deleteItem(root.value, root.right);
+    }
+
+    return root;
+  }
+
+  minValue(root) {
+    let value = root.value;
+    while (root.left != null) {
+      value = root.left.value;
+      root = root.left;
+    }
+    return value;
+  }
 }
 
 const test = [1, 7, 4, 23, 8, 3, 9, 4, 5, 7, 9, 67, 6345, 324];
 const balancedTree = new Tree(test);
+prettyPrint(balancedTree.root);
+balancedTree.insert(6);
+prettyPrint(balancedTree.root);
+balancedTree.deleteItem(4);
 prettyPrint(balancedTree.root);
